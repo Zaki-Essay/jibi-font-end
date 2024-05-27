8ec6a2b3e33dfdda9df76f853708ec041c97e8f6
Zaki-Essay <zakaressaiydy@gmail.com>
2024-05-27 02:45:44 +0100

    JB-2 : shadcn library Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
-  return (
+    function handleOnClick() {
+        alert("shad cn work !")
+    }
+
+    return (
     <>
      <h1 className="text-3xl font-bold underline bg-red-200">
           Hello world!
     </h1>
+        <Button onClick={()=>handleOnClick()} >
+            Test Shad Cn
+        </Button>
     </>
   )
 }
